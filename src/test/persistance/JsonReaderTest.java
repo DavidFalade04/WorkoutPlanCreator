e8d76a6633581f793273b0868224916f4a8ad440
DavidFalade04 <62578996+DavidFalade04@users.noreply.github.com>
2023-03-06 03:12:36 -0800

    wrote tests for JSOn reader

---
+++ src/test/persistance/JsonReaderTest.java
@@ -0,0 +1,72 @@
+package persistance;
+
+import model.Day;
+import model.WorkoutPlan;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class JsonReaderTest {
+List <String> dayNames;
+
+
+        @Test
+        void testReaderNonExistentFile() {
+            JsonReader reader = new JsonReader("./data/noSuchFile.json");
+            try {
+                List <WorkoutPlan> wr = reader.read();
+                fail("IOException expected");
+            } catch (IOException e) {
+                // pass
+            }
+        }
+
+        @Test
+        void testReaderEmptyWorkoutPlan() {
+            JsonReader reader = new JsonReader("data/JsonData/JsonTestFiles/TestNoWorkoutPlans.json");
+            try {
+                List<WorkoutPlan> workoutPlans = reader.read();
+                assertEquals(0, workoutPlans.size());
+            } catch (IOException e) {
+                fail("Couldn't read from file");
+            }
+        }
+
+        @Test
+        void testReaderGeneralWorkoutPlan() {
+            initDefaultDays();
+            JsonReader reader = new JsonReader("data/JsonData/JsonTestFiles/TestWorkoutPlan.json");
+            try {
+                List<WorkoutPlan> plans = reader.read();
+                WorkoutPlan wp = plans.get(0);
+                assertEquals("David's Workout", wp.getName());
+                List<Day> days = new ArrayList<>();
+                for (String d : dayNames) {
+                    Day day = wp.getDay(d);
+                    days.add(day);
+                }
+                assertEquals(7,days.size());
+
+            } catch (IOException e) {
+                fail("Couldn't read from file");
+            }
+        }
+    //Modifies: this
+    //EFFECTS: creates days of the week and initializes names of day
+    private void initDefaultDays() {
+        dayNames = new ArrayList<>();
+        dayNames.add("monday");
+        dayNames.add("tuesday");
+        dayNames.add("wednesday");
+        dayNames.add("thursday");
+        dayNames.add("friday");
+        dayNames.add("saturday");
+        dayNames.add("sunday");
+    }
+
+}
